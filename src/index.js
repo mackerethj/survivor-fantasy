3917dc565de06b05c2fe359db833dc6ce346c431
mackerethj <mackerethj@gmail.com>
2026-02-25 13:45:06 -0500

    Replace React app rendering with HTML template
    Replaced React rendering code with HTML structure.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+    <title>Survivor Fantasy</title>
+  </head>
+  <body>
+    <div id="root"></div>
+  </body>
+</html>
